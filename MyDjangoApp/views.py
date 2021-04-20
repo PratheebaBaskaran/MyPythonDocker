9476e97103a215d249528aeb85c5a926196b2841
PratheebaBaskaran <bpratheeba.12@gmail.com>
2021-04-20 18:56:48 +0530

    test

--- MyDjangoApp/views.py
+++ MyDjangoApp/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Welcome to Pratheeba's Django App in Docker Container using Python!")
+    return HttpResponse("Hello, Welcome to Pratheeba's Django Jenkins App in Docker Container using Python!")
